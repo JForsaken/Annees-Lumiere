b68c66de8fc1d0ba686dd309a8966d9b7ccdcb11
laurent ayoub <technorpm@gmail.com>
2016-08-10 17:19:00 -0400

    added the missing files

---
+++ src/components/home/HomeHeadline.jsx
@@ -0,0 +1,75 @@
+/* Node modules */
+import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import radium from 'radium';
+import { FormattedMessage } from 'react-intl';
+
+/* Components */
+import photoFille from '../../../assets/images/terreReverse.jpg';
+
+/* Styles */
+const style = {
+
+  homeHeadlineBackground: {
+
+    backgroundImage: `url(${photoFille})`,
+    textAlign: 'center',
+    backgroundPosition: 'center 20%',
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+    width: '100%',
+    marginLeft: '0',
+    marginRight: '0',
+    marginTop: '10px',
+    paddingTop: '10%',
+    paddingBottom: '10%',
+    paddingRight: '30px',
+    paddingLeft: '30px',
+    '@media (min-width: 320px)': {
+      height: '300px',
+    },
+    '@media (min-width: 800px)': {
+      height: '800px',
+    },
+  },
+
+  title: {
+    marginTop: 0,
+    height: '100%',
+    fontSize: '4vw',
+    fontWeight: 700,
+  },
+
+  text: {
+    fontSize: '16px',
+    textAlign: 'center',
+    margin: 'auto',
+  },
+
+  transbox: {
+    backgroundColor: '#ffffff',
+    opacity: '0.6',
+    filter: 'alpha(opacity=60)',
+    padding: '40px',
+    paddingTop: '20px',
+  },
+};
+
+@radium
+export default class HomeHeadline extends Component {
+  render() {
+    return (
+      <Row style={style.homeHeadlineBackground}>
+        <Col xs={12} md={6} />
+        <Col xs={12} md={6} style={style.transbox}>
+          <div style={style.title}>
+            <FormattedMessage id="home.homeHeadline.title" />
+          </div>
+          <div style={style.text}>
+            <FormattedMessage id="home.homeHeadline.text" />
+          </div>
+        </Col>
+      </Row>
+    );
+  }
+}
